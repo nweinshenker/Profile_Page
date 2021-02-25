30f7d5f791488dc1934f082e45b85ab1053f9426
nweinshenker <nweinshenker@wisc.edu>
2021-02-24 21:22:40 -0600

    Avatar image for the personal image to show my face

--- src/App.js
+++ src/App.js
@@ -1,9 +1,19 @@
 import './App.css';
+import Avatar from './components/Avatar'
+
+
+const user = {
+  src : "images/cow.jpeg",
+  alt : "this is a cow"
+}
 
 function App() {
   return (
     <div className="App">
-
+      <Avatar
+        src={user.src}
+        alt={user.alt}
+                />
     </div>
   );
 }
--- src/components/Avatar.js
+++ src/components/Avatar.js
@@ -1,14 +1,18 @@
-import React, {Compoent} from 'react'
+import React, { Component } from 'react'
 
-export default class Avatar extends React.Component {
+export default class Avatar extends Component {
     render () {
         return (
             <div className="Avatar">
                 <div className="Image Box">
                     <img className="ProfilePicture" 
-                        src={this.props.imageUrl}
+                        src={this.props.src}
                         alt={this.props.alt}>
                     </img>
+                </div>
+                <div className="container-fluid">
+                    <h1>Favorite Author Quote</h1>
+                    <h2>'“We have to continually be jumping off cliffs and developing our wings on the way down.” -Kurt Vonnegut'</h2>
                 </div>
             </div>
         )
