456266b296bd37a1b3c22ec8636c3f5297eb17bf
nweinshenker <nweinshenker@wisc.edu>
2021-02-25 10:25:49 -0600

    cleanup within the public folder

--- src/App.js
+++ src/App.js
@@ -1,19 +1,23 @@
 import './App.css';
 import Avatar from './components/Avatar'
+import SideBar from './components/SideBar';
 
 
 const user = {
-  src : "images/cow.jpeg",
-  alt : "this is a cow"
+  src: "images/cow.jpeg",
+  alt: "this is a cow"
 }
 
 function App() {
   return (
     <div className="App">
+      {/*
       <Avatar
         src={user.src}
         alt={user.alt}
-                />
+      />
+      */}
+      <SideBar />
     </div>
   );
 }
